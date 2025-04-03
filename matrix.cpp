3b55814f3642326218bc4811259a49097e1f7c8a
Daul Lee <daulplee12624120@gmail.com>
2025-04-03 10:08:14 -0500

    fixed get size, added swaps

--- matrix.cpp
+++ matrix.cpp
@@ -6,11 +6,16 @@
 // colon after parameter list of a constructor is a list of member
 // variables and what to initialize them to
 Matrix::Matrix(std::size_t size) : _size(size) {
-
+    for (int i = 0; i < _size; i++) {
+        _matrix.push_back(std::vector<int> ());
+        for (int j = 0; j < _size; j++) {
+            _matrix.at(i).push_back(0);
+        }
+    }
 }
 
 Matrix::Matrix(std::vector<std::vector<int>> matrix) : _matrix(matrix) {
-    std::vector<std::vector<int>> _matrix(_size, std::vector<int>(_size));
+
 }
 
 Matrix Matrix::operator+(const Matrix &rhs) const {
@@ -42,6 +47,10 @@ int Matrix::get_value(std::size_t i, std::size_t j) const{
     return _matrix.at(i).at(j);
 }
 
+int Matrix::get_size() const {
+    return _size;
+}
+
 void Matrix::set_value(std::size_t i, std::size_t j, int k) {
     _matrix.at(i).at(j) = k;
 }
@@ -63,12 +72,25 @@ int Matrix::sum_diagonal_minor() const {
 };
 
 void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
-
-
+    int value1; 
+    int value2;
+    for (int i = 0; i < _size; i++) {
+        value1 = this->get_value(r1, i);
+        value2 = this->get_value(r2, i);
+        this->set_value(r1, i, value2);
+        this->set_value(r2, i, value1);
+    }
 };
 
 void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
-
+    int value1; 
+    int value2;
+    for (int i = 0; i < _size; i++) {
+        value1 = this->get_value(i, c1);
+        value2 = this->get_value(i, c2);
+        this->set_value(i, c1, value2);
+        this->set_value(i, c2, value1);
+    }
 }
 
 void Matrix::print_matrix() const {
